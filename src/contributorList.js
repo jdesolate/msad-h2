927b64b4704081f3ea9acf2218f5f08fcd8b3c3c
Merv Tampus <79685609+jdesolate@users.noreply.github.com>
2024-07-02 00:19:41 +0800

    [Feature][Merv] Add project list data

--- src/contributorList.js
+++ src/contributorList.js
@@ -1,25 +1,29 @@
-import { github_usernames } from "./constants.js";
+import { github_usernames, projects } from "./constants.js";
 
 const container = document.getElementById("contributors-list");
 
-github_usernames.forEach((username, key) => {
+Object.values(github_usernames).forEach((username, index) => {
   const contributor = username;
+
+  const projectName = projects.find((project) =>
+    project.contributors.includes(contributor),
+  )?.name;
 
   const a = document.createElement("a");
   a.href = `https://github.com/${contributor}`;
-  a.id = key;
+  a.id = index;
   a.target = "_blank";
 
   const img = document.createElement("img");
   img.src = `https://github.com/${contributor}.png`;
-  img.alt = key;
-  img.id = `img-${key}`;
+  img.alt = index;
+  img.id = `img-${index}`;
 
   const h4 = document.createElement("h4");
   h4.textContent = username;
 
   const p = document.createElement("p");
-  p.textContent = `Contributor ${key + 1}`;
+  p.textContent = projectName ?? "MSAD-H2";
 
   a.appendChild(img);
   a.appendChild(h4);
--- src/projectList.js
+++ src/projectList.js
@@ -5,3 +5,32 @@
   <p>Project 1 description</p>
 </a>; */
 }
+import { projects } from "./constants.js";
+
+const container = document.getElementById("projects-list");
+
+Object.values(projects).forEach((project, index) => {
+  const projectName = project.name;
+
+  const a = document.createElement("a");
+  a.href = `https://github.com/${projectName}`;
+  a.id = index;
+  a.target = "_blank";
+
+  const img = document.createElement("img");
+  img.src = `https://picsum.photos/${index + 200}/${index + 200}`;
+  img.alt = index;
+  img.id = `img-${index}`;
+
+  const h4 = document.createElement("h4");
+  h4.textContent = projectName;
+
+  const p = document.createElement("p");
+  p.textContent = "Ongoing Project";
+
+  a.appendChild(img);
+  a.appendChild(h4);
+  a.appendChild(p);
+
+  container.appendChild(a);
+});
